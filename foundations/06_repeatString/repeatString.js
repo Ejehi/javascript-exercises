a084a416fffa79bf5bb0cef8576fa2337a7eab16
Ejehi <ijezry@gmail.com>
2026-01-02 01:46:37 +0100

    Implement solutions for loops and array exercises
    Added implementations for repeatString, reverseString, removeFromArray, sumAll, leapYears, and temperature conversion functions. Updated corresponding test files to enable and verify all test cases. These changes complete the core logic for the exercises and ensure all tests are passing.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, timesToRepeat) {
+    let repeatedString = '';
+    if(timesToRepeat < 0) return 'ERROR';
+    for (let i = 0; i < timesToRepeat; i++) {
+        repeatedString += word;
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInCelsius = Math.round(((temp - 32) * (5/9)) * 10)/10;
+  return tempInCelsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInFahrenheit = Math.round(((temp * 9/5) + 32) * 10)/10;
+  return tempInFahrenheit; 
 };
 
 // Do not edit below this line
