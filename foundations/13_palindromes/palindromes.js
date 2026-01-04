d13b63e563a4e46ae812daff00b06e4d04ec31b5
Ejehi <ijezry@gmail.com>
2026-01-04 22:59:43 +0100

    Implement calculator and palindrome functions
    Completed the calculator functions (add, subtract, sum, multiply, power, factorial) and enabled their corresponding tests. Implemented the palindromes function to check for palindromic strings and enabled all related tests.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    if(word.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).reverse().join("") 
+        === word.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).join("")) return true;
+    return false;
 
 };
 
